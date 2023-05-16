804af5ea73a05a7f09dbf7915fdede184288917a
Martins Gm <swanta2002@yahoo.com>
2023-05-16 18:22:39 +0200

    MySQL Sakila_Task1

---
+++ Task1/Mysql_Sakila_TASK1.sql
@@ -0,0 +1,18 @@
+USE sakila;
+SELECT *
+FROM actor, film , customer;
+
+SELECT title
+FROM film;
+
+SELECT *
+FROM language;
+
+SELECT COUNT(store_ID)
+FROM store;
+
+SELECT COUNT(store_ID)
+FROM staff;
+
+SELECT first_name
+FROM staff;
